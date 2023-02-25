a4c150cd1b1dab744b0390d86902cf557c0f757f
이진희 <49019236+bebusl@users.noreply.github.com>
2023-02-26 07:41:03 +0900

    feat: 로그아웃 기능 추가

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -2,8 +2,9 @@ import React, { useEffect, useState } from "react";
 import DepositGrid from "../../components/DepositGrid";
 import { Button, Card, Layout, Space, Statistic } from "antd";
 import { LinkOutlined, LogoutOutlined } from "@ant-design/icons";
-import { useLocation } from "react-router-dom";
+import { Navigate, useLocation } from "react-router-dom";
 import { dashboardRequest } from "../../api/transactionAPI";
+import { logoff } from "../../utils";
 const { Header, Content } = Layout;
 
 const headerStyle = {
@@ -27,9 +28,6 @@ const CustomCard = ({
     <Statistic title={title} value={value} prefix={<LinkOutlined />} />
   </Card>
 );
-// http://172.30.1.17:8080/api/v1/subscribe "/api/v1/subscribe", Authorization: 넣어주기
-// "tx"를 subscribe
-// 검색중일때는 추가할 필요X
 
 function Main() {
   const location = useLocation();
@@ -51,9 +49,12 @@ function Main() {
         console.error("FAILED TO FETCH DASHBOARD DATA");
       }
     })();
-
-    // return () => eventSource.curren
   }, []);
+
+  if (!userName) {
+    window.alert("로그아웃 되었습니다.");
+    return <Navigate to="/login"></Navigate>;
+  }
 
   return (
     <Layout>
@@ -66,6 +67,7 @@ function Main() {
               type="text"
               icon={<LogoutOutlined />}
               style={{ color: "white" }}
+              onClick={logoff}
             >
               로그아웃
             </Button>
