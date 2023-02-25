05e12c86aed71868bfabbb9cf9dc7e47a47cde0c
이진희 <49019236+bebusl@users.noreply.github.com>
2023-02-25 22:59:00 +0900

    feat: antd global theme provider 설정

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "antd/dist/reset.css";
+import { ConfigProvider, theme } from "antd";
+import { customTheme } from "./style/theme";
+import "./App.css";
+
+import DefaultRouter from "./DefaultRouter";
+
+const { defaultAlgorithm } = theme;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ConfigProvider
+      theme={{
+        ...customTheme,
+        algorithm: defaultAlgorithm,
+      }}
+    >
+      <DefaultRouter />
+    </ConfigProvider>
   );
 }
 
---
+++ src/style/theme.js
@@ -0,0 +1,9 @@
+export const customTheme = {
+  token: {
+    fontFamily: "Avenir, Helvetica, Arial, sans-serif",
+    fontSize: 16,
+    fontSizeSM: 15,
+  },
+};
+
+export const theme = {};
